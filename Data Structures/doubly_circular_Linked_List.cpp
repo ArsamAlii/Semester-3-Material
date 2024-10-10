4f763cfeb56c1efe71b71a4d9c1690d2c21bb64b
ArsamAli <161602936+ArsamAlii@users.noreply.github.com>
2024-10-10 22:45:31 +0500

    doubly_circular_Linked_List.cpp
    Insert, Remove, Display function in doubly linked list

--- Data Structures/doubly_circular_Linked_List.cpp
+++ Data Structures/doubly_circular_Linked_List.cpp
@@ -17,6 +17,7 @@ class Node{
 };
 class D_linkedlist{
     Node *head;
+    int length=0;
     public:
     D_linkedlist(){
         head=NULL;
@@ -38,6 +39,47 @@ class D_linkedlist{
             newnode->next=head;
             head->prev=newnode;
         }
+        length++;
+    }
+
+    void remove(int pos){
+        if(head==NULL){
+            cout<<"list is empty nothing to remove"<<endl;
+            return;
+        }
+        else if(pos<1||pos>length){
+            cout<<"Invalid position cannot remove ..."<<endl;
+            return;
+        }
+        else{
+            Node *curr=head;
+            if(pos==1){
+                Node *temp=head;
+                while(temp->next!=head){
+                    temp=temp->next;
+                }
+                if(head->next==head){
+                    delete head;
+                    head=NULL;
+                }else{
+                    head=head->next;
+                    head->prev=temp;
+                    temp->next=head;
+                    delete curr;
+                }
+            }else{
+                Node *slow=head;
+                for(int i = 0 ; i<pos-1;i++){
+                    slow=curr;
+                    curr=curr->next;
+                }
+                slow->next=curr->next;
+                curr->next->prev=slow;
+            }            
+            delete curr;
+            length--;
+        }
+
     }
 
     void display(){
@@ -61,5 +103,9 @@ int main(){
 
     l1.display();
 
+
+    l1.remove(3);
+
+    l1.display();
     return 0;
 }
